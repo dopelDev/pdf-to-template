0c4679f3e31f50b74c4825f8c01d3ddffd5cb3a5
dopeldev <322kuroneko@gmail.com>
2024-03-08 20:55:26 -0500

    work multiple files

--- pdf-to-template/app.py
+++ pdf-to-template/app.py
@@ -1,87 +1,74 @@
-from flask import Flask, request, redirect, url_for, render_template, jsonify
+from flask import Flask, request, redirect, url_for, render_template, session
 import os
 import json
 from werkzeug.utils import secure_filename
 from mSousa.main_module import main as mSousa_main
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 app.config['UPLOAD_FOLDER'] = './pdf_directory'  # Actualiza esto con la ruta donde quieres guardar los archivos
 app.config['IMAGES_FOLDER'] = './images'  # Actualiza esto con la ruta donde quieres guardar las imagenes
 app.config['RESPONSE_FOLDER'] = './responses'  # Actualiza esto con la ruta donde quieres guardar las respuestas
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
-@app.route('/upload', methods=['GET', 'POST'])
+from flask import session
+
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    duplicate_names = None
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return "No file part", 400
+        files = request.files.getlist('file')
+        if not files or files[0].filename == '':
+            return "No selected files", 400
 
-        file = request.files['file']
+        # Lista para almacenar nombres de los archivos procesados
+        processed_files = []
 
-        if file.filename == '':
-            return "No selected file", 400
-        filename = file.filename.replace(" ", "_")
-        if file:
-            filename = secure_filename(filename)  # Use the modified filename
+        for file in files:
+            filename = secure_filename(file.filename.replace(" ", "_"))
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-            app.config['RESPONSE_FOLDER'] = os.path.join(app.config['RESPONSE_FOLDER'])
-            data, duplicate_names = mSousa_main(file_path, app.config['RESPONSE_FOLDER'], app.config['IMAGES_FOLDER']) 
+
+            data = mSousa_main(file_path, app.config['RESPONSE_FOLDER'], app.config['IMAGES_FOLDER'])
             json_data = json.dumps(data)
 
-            # Guarda los resultados en un archivo
             filename_without_ext = os.path.splitext(filename)[0]
-            app.config['PROCESSED_FOLDER'] = os.path.join(app.config['RESPONSE_FOLDER'], filename_without_ext, 'processed')
+            processed_folder = os.path.join(app.config['RESPONSE_FOLDER'], filename_without_ext, 'processed')
+            if not os.path.exists(processed_folder):
+                os.makedirs(processed_folder)
+
             results_filename = filename_without_ext + '.json'
-            # create processed folder directory
-            if not os.path.exists(app.config['PROCESSED_FOLDER']):
-                os.makedirs(app.config['PROCESSED_FOLDER'])
-            results_filepath = os.path.join(app.config['PROCESSED_FOLDER'], results_filename)
+            results_filepath = os.path.join(processed_folder, results_filename)
             with open(results_filepath, 'w') as f:
                 f.write(json_data)
 
-            if duplicate_names is not None:
-                json_data_duplicate_names = json.dumps(duplicate_names)
-                results_filename_duplicate_names = filename_without_ext + '_duplicate_names.json'
-                results_filepath_duplicate_names = os.path.join(app.config['PROCESSED_FOLDER'], results_filename_duplicate_names)
-                with open(results_filepath_duplicate_names, 'w') as f:
-                    f.write(json_data_duplicate_names)
-                
-                # Redirige al usuario a la URL de resultados
-                return redirect(url_for('show_results_with_duplicates', filename=results_filename, filename_duplicates=results_filename_duplicate_names))
+            # Agrega el nombre del archivo procesado a la lista
+            processed_files.append(results_filename)
 
-            return redirect(url_for('show_results', filename=results_filename))
+        # Almacena los nombres de los archivos procesados en la sesión
+        session['processed_files'] = processed_files
 
-    else:
-        return render_template('upload.html')
+        # Redirige al usuario a la página de resultados
+        return redirect(url_for('show_results'))
 
-@app.route('/results/<filename>', methods=['GET'])
-def show_results(filename):
-    # Carga los resultados del archivo
-    # Guarda los resultados en un archivo
-    # create processed folder directory
-    if not os.path.exists(app.config['PROCESSED_FOLDER']):
-                os.makedirs(app.config['PROCESSED_FOLDER'])
-    filename_without_ext = os.path.splitext(filename)[0]
-    results_filepath = os.path.join(app.config['PROCESSED_FOLDER'], filename_without_ext)
-    with open(results_filepath, 'r') as f:
-        results = f.read()
-    return render_template('report.html', data=json.loads(results))
+    return render_template('upload.html')
 
-@app.route('/results/<filename>/<filename_duplicates>', methods=['GET'])
-def show_results_with_duplicates(filename, filename_duplicates):
-    # Carga los resultados del archivo
-    results_filepath = os.path.join(app.config['PROCESSED_FOLDER'], filename)
-    with open(results_filepath, 'r') as f:
-        results = f.read()
-    # Carga los resultados del archivo
-    results_filepath_duplicates = os.path.join(app.config['PROCESSED_FOLDER'], filename_duplicates)
-    with open(results_filepath_duplicates, 'r') as f:
-        results_duplicates = f.read()
-    # Find duplicates
-    return render_template('report_with_duplicates.html', data=json.loads(results), duplicate_names=json.loads(results_duplicates))
+@app.route('/results', methods=['GET'])
+def show_results():
+    # Recupera los nombres de los archivos procesados de la sesión
+    json_filenames = session.get('processed_files', [])
+
+    datasets = []
+    for filename in json_filenames:
+        processed_folder = os.path.join(app.config['RESPONSE_FOLDER'], os.path.splitext(filename)[0], 'processed')
+        results_filepath = os.path.join(processed_folder, filename)
+        with open(results_filepath, 'r') as f:
+            datasets.append(json.load(f))
+    return render_template('report.html', datasets=datasets)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
 def allowed_file(filename):
--- pdf-to-template/mSousa/main_module.py
+++ pdf-to-template/mSousa/main_module.py
@@ -9,55 +9,56 @@ from .submodule import proc_pdf, call_to_ocr, proc_json
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = f'{os.getcwd()}/cosmic-octane-402721-14cfb94c3c72.json'
 credentials_file = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 
-def main(file, responses_path, images_path):
-    same_names = None
+def processing_data(file, responses_path, images_path):
     pdf_name = proc_pdf.convert_pdf_to_images(file, images_path)
     if not os.path.exists(f"{responses_path}"):
         os.makedirs(f"{responses_path}")
     call_to_ocr.ocr_processing(credentials_file, images_path, f"{responses_path}/", pdf_name)
     final_json = []
 
-    if os.path.exists(responses_path):
-       print(f"processing {responses_path}/from_OCR_{pdf_name}/{pdf_name}/")
-       json_files = glob.glob(f"{responses_path}/{pdf_name}/from_OCR_{pdf_name}/*.json")
-       json_files.sort(key=os.path.getsize, reverse=True)
-       print(f"processing {json_files} json files")
-       first_iteration = True
-       for file in json_files:
-           print(f"Processing json file: {file}")
-           with open(file, 'r') as f:
-               json_data = json.load(f)
-           if first_iteration:
-               words = ['Employee', 'Retention', 'credit']
-               coords = proc_json.find_coordinates(json_data, words)
-               company_name = proc_json.find_company_name(json_data, coords) 
-               company_name = proc_json.clean_company_name(company_name)
-               print(f'company_name: {company_name}')
-   
-               first_iteration = False
-               # Encuentra X1, X2 y la menor Y1 para "Master" y "tabs"
-               words = ["Master", "Employee", "List"]
-               coords = proc_json.find_coordinates(json_data, words)
-   
-               # Encuentra todos los textos que están debajo de "Master" y "tabs" y agrúpalos usando los valores obtenidos
-               names = proc_json.find_in_all_y(json_data, coords)
-               names = proc_json.transform_structure(names)
-               all_columns = proc_json.find_in_all_x(json_data, names)
-               company_info = {'Company name': company_name}
-               print(f'company_info: {company_info}')
-               all_columns.insert(0, company_info)
-               final_json = all_columns
-           else:
-               print(f'processing json file {file}')
-               words = ['DocuSign', 'Envelope']
-               coords = proc_json.find_coordinates(json_data, words)
-               names = proc_json.find_in_all_y(json_data, coords)
-               names = proc_json.transform_structure(names)
-               all_columns = proc_json.find_in_all_x(json_data, names)
-               final_json.extend(all_columns)
-       print('saving to json extructure')
-       same_names = proc_json.agroup_duplicated_names(final_json)
-       with open(f'{responses_path}/{pdf_name}/extructure.json', 'w') as f:
-           json.dump(final_json, f, indent=4)
+    if os.path.exists(f"{responses_path}/{pdf_name}/from_OCR_{pdf_name}"):
+        json_files = glob.glob(f"{responses_path}/{pdf_name}/from_OCR_{pdf_name}/*.json")
+        json_files.sort(key=os.path.getsize, reverse=True)
+        first_iteration = True
+        for file in json_files:
+            with open(file, 'r') as f:
+                json_data = json.load(f)
+            if first_iteration:
+                words = ['Employee', 'Retention', 'credit']
+                coords = proc_json.find_coordinates(json_data, words)
+                company_name = proc_json.find_company_name(json_data, coords) 
+                company_name = proc_json.clean_company_name(company_name)
 
-    return final_json, same_names
+                first_iteration = False
+                words = ["Master", "Employee", "List"]
+                coords = proc_json.find_coordinates(json_data, words)
+
+                names = proc_json.find_in_all_y(json_data, coords)
+                names = proc_json.transform_structure(names)
+                all_columns = proc_json.find_in_all_x(json_data, names)
+                company_info = {'Company name': company_name}
+                all_columns.insert(0, company_info)
+                final_json = all_columns
+            else:
+                words = ['DocuSign', 'Envelope']
+                coords = proc_json.find_coordinates(json_data, words)
+                names = proc_json.find_in_all_y(json_data, coords)
+                names = proc_json.transform_structure(names)
+                all_columns = proc_json.find_in_all_x(json_data, names)
+                final_json.extend(all_columns)
+
+        with open(f'{responses_path}/{pdf_name}/extructure.json', 'w') as f:
+            json.dump(final_json, f, indent=4)
+
+    return final_json
+
+def main(input_data, responses_path, images_path):
+    if isinstance(input_data, list):
+        final_results = []
+        for file in input_data:
+            result = processing_data(file, responses_path, images_path)
+            final_results.append(result)
+    else:
+        final_results = processing_data(input_data, responses_path, images_path)
+
+    return final_results
